429d2ccc7e5ac3a61549cc738854e6d65fd3d1a3
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-11 14:34:07 +0530

    test

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,11 +1,32 @@
 import { Link } from "react-router-dom";
+import Map from 'immutable';
+
+// remaining topics here - 
+// ImmutableJS https://www.npmjs.com/package/immutable#getting-started 
+
+
+
+
 
 const Page404 = () => {
+
+    const map1 = Map({ a: 1, b: 2, c: 3 });
+    const map2 = map1.set('b', 50);
+    // map1.get('b') + ' vs. ' + map2.get('b');
+
+
     return (
         <div className="container">
             <h1>Oops!</h1>
             <p>The page you are looking for is not found.</p>
             <Link className="btn btn-primary" to="/home" >Let's go home...</Link>
+
+            <div>
+                <p>ImmutableJS</p>
+                <p>{map1}</p>
+                <p>{map2}</p>
+            </div>
+
 
         </div>
     );
